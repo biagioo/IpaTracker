13ff62a761ec19330996656f9b5de723f4046c7d
biagioo <mendolia2328@gmail.com>
2020-05-06 13:21:22 -0400

    created new ipa and show action, adjusted views appropriately

--- app/controllers/ipas_controller.rb
+++ app/controllers/ipas_controller.rb
@@ -1,29 +1,38 @@
 class IpasController < ApplicationController
-
+    before_action :logged_in?, :current_ipa, except: [:new, :create]
     def index
+        redirect_to new_user_path unless logged_in?
         @ipas = Ipa.all
     end
 
     def new 
-
+        @ipa = Ipa.new
     end
 
     def create
-
+        @ipa = Ipa.create(ipa_params)
+        if @ipa.valid?
+            flash[:notice] = "IPA Creation Successful!"
+            redirect_to @ipa
+        else
+            render :new
+        end
     end
     
-    def show
-
-    end
-
-    def edit
-
-    end
-
     def update
 
     end
 
     def destroy
+    end
+
+    private
+
+    def current_ipa
+        @ipa = Ipa.find_by(id: params[:id])
+    end
+
+    def ipa_params
+        params.require(:ipa).permit(:name, :brand, :size)
     end
 end
--- app/models/ipa.rb
+++ app/models/ipa.rb
@@ -2,4 +2,5 @@ class Ipa < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
     validates :name, uniqueness: true
+    validates :name, presence: true
 end
