c6005ccd0bbee8a3d72291a6d8450f4bf869d5ba
biagioo <mendolia2328@gmail.com>
2020-05-06 12:47:37 -0400

    create functional IPA index action

--- app/controllers/ipas_controller.rb
+++ app/controllers/ipas_controller.rb
@@ -1,2 +1,29 @@
 class IpasController < ApplicationController
+
+    def index
+        @ipas = Ipa.all
+    end
+
+    def new 
+
+    end
+
+    def create
+
+    end
+    
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+    end
 end
