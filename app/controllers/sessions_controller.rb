967ccfda296705161b1102a814fd7d5c73bde34b
biagioo <mendolia2328@gmail.com>
2020-05-07 08:38:37 -0400

    displayed error message on sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         redirect_to user_path(user)
       else
+        flash[:notice] = "Email or password is invalid. Please try agian."
         render :new
       end
     end
