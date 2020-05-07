14d001c30baf95523d5a2b8dbdd99790a613e1e2
biagioo <mendolia2328@gmail.com>
2020-05-07 09:50:31 -0400

    Ipa reviews index page created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,16 @@ class ReviewsController < ApplicationController
     before_action :logged_in?   
 
     def index
-        byebug
+        if params[:ipa_id]
+            @ipa = Ipa.find_by(id: params[:ipa_id]) 
+            if @ipa 
+                @reviews = @ipa.reviews 
+            else
+                redirect_to ipas_path, alert: "Ipa not found"
+            end
+        else
+            redirect_to ipas_path
+        end
     end
 
 end
