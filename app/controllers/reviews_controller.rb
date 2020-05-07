6b9f1764686e69d2708f6aaf577e7e01f3869a89
biagioo <mendolia2328@gmail.com>
2020-05-07 11:22:25 -0400

    review creation is now functional

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-    before_action :logged_in?   
+    before_action :logged_in?, :current_user   
 
     def index
         if params[:ipa_id]
@@ -13,10 +13,38 @@ class ReviewsController < ApplicationController
             redirect_to ipas_path
         end
     end
+    
+    def new 
+        if params[:ipa_id]
+            @ipa = Ipa.find_by(id: params[:ipa_id]) 
+            @review = @ipa.reviews.new(user_id: @user.id)
+        end
+    end
+
+    def create
+        @ipa = Ipa.find_by(id: params[:ipa_id]) 
+        @review = @ipa.reviews.create(review_params)
+        if @review.valid? 
+            @review.save
+            @ipa = @review.ipa
+            redirect_to ipa_review_path(@ipa, @review)
+        else
+            byebug
+        end
+
+       
+    end
 
     def show
         @ipa = Ipa.find_by(id: params[:ipa_id])
         @review = @ipa.reviews.find_by(id: params[:id])
+    end
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content, :user_id)
     end
 
 end
