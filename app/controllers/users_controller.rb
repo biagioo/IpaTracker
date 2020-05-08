dcbeb1b3ae0d81c743573783291a6b5f27029cfc
biagioo <mendolia2328@gmail.com>
2020-05-08 08:10:59 -0400

    added admin authorization to deleting IPAs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password, :email, :bio)
+        params.require(:user).permit(:username, :password, :email, :bio, :admin)
     end 
 end
